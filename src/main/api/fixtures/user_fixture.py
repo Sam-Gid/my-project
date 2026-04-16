193589cb4600bdb8860d79c3d367b0059c32b0fb
Sam-Gid <hesoyamim707@gmail.com>
2026-04-16 17:57:10 +0300

    TestCommitUpperCASE

--- src/main/api/fixtures/user_fixture.py
+++ src/main/api/fixtures/user_fixture.py
@@ -12,9 +12,9 @@ from src.main.api.models.create_user_request import CreateUserRequest
 @pytest.fixture
 def create_user_request(api_manager):
     # Создаем обычного пользователя (ROLE_USER).
-    create_user_request = RandomModelGenerator.generate(CreateUserRequest)
-    api_manager.admin_steps.create_user(create_user_request)
-    return create_user_request
+    user_request = RandomModelGenerator.generate(CreateUserRequest)
+    api_manager.admin_steps.create_user(user_request)
+    return user_request
 
 
 @pytest.fixture
