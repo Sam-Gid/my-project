84e56e40835e180c6189c0329f16ae6b8dd9a0c1
Sam-Gid <hesoyamim707@gmail.com>
2026-04-23 16:29:44 +0300

    PR improvements

--- src/main/api/tests/login_user_test.py
+++ src/main/api/tests/login_user_test.py
@@ -7,11 +7,11 @@ from src.main.api.models.login_user_request import LoginUserRequest
 class TestUserLogin:
     def test_login_admin(self, api_manager: ApiManager, login_admin_request: LoginUserRequest):
         response = api_manager.admin_steps.login_user(login_admin_request)
-        assert response.user.role == 'ROLE_ADMIN', 'Ошибка: Роль пользователя не соответствует запросу'
+        assert response.user.role == "ROLE_ADMIN", "Ошибка: Роль пользователя не соответствует запросу"
 
 
     def test_login_user(self, api_manager, create_user_request):
         response = api_manager.admin_steps.login_user(create_user_request)
         assert create_user_request.username == response.user.username, (
-            'Ошибка: Имя пользователя не соответствует запросу'
+            "Ошибка: Имя пользователя не соответствует запросу"
         )
